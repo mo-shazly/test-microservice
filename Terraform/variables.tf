aa5602f2e933f0a875397a5bea04a2ba61dbf798
mohamed elshazly <38785121+mo-shazly@users.noreply.github.com>
2024-11-28 10:42:49 +0200

    test

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,33 +1,19 @@
-
 variable "aws_region" {
-  description = "The AWS region to deploy resources in"
+  description = "The AWS region to create resources in"
   type        = string
-  default     = "us-west-2"
 }
-
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
-
 
 variable "public_subnet_a_cidr" {
-  description = "CIDR block for public subnet A"
+  description = "The CIDR block for the public subnet in availability zone A"
   type        = string
-  default     = "10.0.1.0/24"
 }
-
 
 variable "public_subnet_b_cidr" {
-  description = "CIDR block for public subnet B"
+  description = "The CIDR block for the public subnet in availability zone B"
   type        = string
-  default     = "10.0.2.0/24"
 }
-
-
-
-
-
-
