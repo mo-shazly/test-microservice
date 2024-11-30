dc9d663d6b2fb670ec84e4a648562ffb327f9b4c
mohamed elshazly <38785121+mo-shazly@users.noreply.github.com>
2024-11-30 13:48:22 +0200

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_route_table" "public_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.stage_gw1.id
+    gateway_id = aws_internet_gateway.stage-gw1.id
   }
 
   tags = {
