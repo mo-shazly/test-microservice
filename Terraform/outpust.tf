2216fc556625f49d0ec2af52622a79c680e63952
mohamed elshazly <38785121+mo-shazly@users.noreply.github.com>
2024-11-30 04:53:49 +0200

    h

--- Terraform/outpust.tf
+++ Terraform/outpust.tf
@@ -10,23 +10,4 @@ output "eks_cluster_certificate_authority_data" {
   value = aws_eks_cluster.stage_eks.certificate_authority[0].data
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "private_subnet_ids" {
-  value = module.vpc.private_subnets
-}
-
-output "public_subnet_ids" {
-  value = module.vpc.public_subnets
-}
-
-output "eks_nodegroup_arn" {
-  value = aws_eks_node_group.stage_eks_node_group.arn
-}
-
-output "internet_gateway_id" {
-  value       = local.internet_gateway_id != null ? local.internet_gateway_id : aws_internet_gateway.stage-gw[0].id
-  description = "The ID of the Internet Gateway, if any exists."
-}
+output "v[_{{{CITATION{{{_1{](https://github.com/praveenjirra/deploy-docker-swarm-using-terraform-ansible/tree/e051f433f9c3d73c12698a6742dce8ae19bbb07a/README.md)
