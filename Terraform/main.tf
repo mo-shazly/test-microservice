98c93f3f986779d18d37cde5593a8404a25502e6
mohamed elshazly <38785121+mo-shazly@users.noreply.github.com>
2024-11-30 14:29:10 +0200

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "stagebucket12"
-    key            = "stage-eks/terraform.tfstate"
+    key            = "stage-eks1/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock"
     encrypt        = true
