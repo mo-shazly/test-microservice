b77e287769a7d2fe3aa78856f16be2918fcbe559
mohamed elshazly <38785121+mo-shazly@users.noreply.github.com>
2024-12-01 00:46:56 +0200

    n

--- Terraform/outpust.tf
+++ Terraform/outpust.tf
@@ -7,13 +7,25 @@ output "eks_cluster_endpoint" {
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = aws_vpc.stage_vpc.id
 }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnets
+
+output "cluster_id" {
+  value = aws_eks_cluster.stage_eks.id
 }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnets
+output "node_group_id" {
+  value = aws_eks_node_group.stage_eks.id
+}
+
+
+output "subnet_ids" {
+  value = aws_subnet.stage_subnet[*].id
+}
+
+
+output "cluster_token" {
+  value = data.aws_eks_cluster_auth.stage_eks.token
+  sensitive = true
 }
